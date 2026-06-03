29325e6cda3980dcc7bb3c803219330c60b249f9
Riham Mohamed <moriham190@gmail.com>
2026-06-03 15:32:42 +0200

    Initial Angular Project Setup with Bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Hero } from './hero/hero';
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
+import { About } from './about/about';
+import { Projects } from './projects/projects';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Hero, Navbar, Footer, About, Projects],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('my-angular-app');
